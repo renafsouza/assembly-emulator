2afde4e8605bbed6cfee32ab4e37028a6fbcf97d
Gerson Menezes <48692431+GersonMenezes@users.noreply.github.com>
2021-09-19 15:49:51 -0300

    Substituição de parte de operaçao resolvida

--- Macro.java
+++ Macro.java
@@ -17,7 +17,6 @@ public class Macro {
     }
 
     public void addInstrucao(String linha){
-        //System.out.println("\nAddInstrução... \n" + linha);
         this.instrucoes.add(linha);
     }
     
@@ -51,8 +50,6 @@ public class Macro {
         String[] words = callInstruction.replaceAll(","," ").split("\\s+");
         String[] parametrosReais = Arrays.copyOfRange(words, 1, words.length);
         ArrayList<String> ins = new ArrayList<>(this.instrucoes);
-        System.out.println("\nParametros Formais concatenados: " + Arrays.toString(parametrosFormais) + "\n");
-        System.out.println("\nParametros Reais concatenados: " + Arrays.toString(parametrosReais) + "\n");
         
         for(int i=0; i<this.instrucoes.size(); i++){ // Percorre cada instrução da macro
 
@@ -62,39 +59,30 @@ public class Macro {
 
             for(int j=0; j<vars.length; j++){ // Percorre cada variavel da instrução
 
-                if(registers.contains(vars[j])){ //Se a variável é um registrador, pula.
-                    //System.out.println("\nYes, Register...\n" + vars[j]);
-                }
+                if(registers.contains(vars[j])){} //Se a variável é um registrador, pula.
+                
                 else{
                     boolean flagParametrosReal = false;
                     for(int k=0; k<parametrosFormais.length; k++){ // Percorre cada parametro formal
                         if(vars[j].equals(this.parametrosFormais[k])){
-                          
-                            ins.set(i, ins.get(i).replaceAll(this.parametrosFormais[k], parametrosReais[k]));
-                            //System.out.println("Depois: " + this.parametrosFormais[k]);
+                            
+                            ins.set(i, ins.get(i).replaceAll("(?<=[\\s,]|^)"+this.parametrosFormais[k]+"(?=[\\s,]|$)", parametrosReais[k]));
                             flagParametrosReal = true;
                         }
                     }
-                    if(!flagParametrosReal && nivel != 0){
+                    if(!flagParametrosReal && nivel != 0){ // Caso o parametro formal seja de outra Macro de nivel superior
 
-                        setOldParametros(newMap, vars[j]);
+                        setOldParametros(newMap, vars[j]); 
                         if(!oldParametrosFormais.equals("")){
                             String[] wordsFormais = oldParametrosFormais.replaceAll(";"," ").split("\\s+");
                             String[] wordsReais = oldParametrosReais.replaceAll(";"," ").split("\\s+");
-                            
-                            System.out.println("\nWordsFormas: " + Arrays.toString(wordsFormais) + "\n");
-                            System.out.println("\nWordsReais: " + Arrays.toString(wordsReais) + "\n");
-                            System.out.println("\nVar: " + Arrays.toString(vars) + "\n");
 
                             for(int l = wordsFormais.length - 1; l >= 0; l--){
-                                System.out.println("\nPilha: " + wordsFormais[l] + "\n");
                                 if(vars[j].equals(wordsFormais[l])){
-                                    System.out.println("\nEntrando aqui: é " + wordsReais[l]);
-                                    ins.set(i, ins.get(i).replaceAll(vars[j], wordsReais[l]));
+                                    ins.set(i, ins.get(i).replaceAll("(?<=[\\s,]|^)"+vars[j]+"(?=[\\s,]|$)", wordsReais[l]));
                                 }
                             }
-                        }
-                        
+                        } 
                     }
                 }   
             }
@@ -104,17 +92,15 @@ public class Macro {
         return ins;
     }
     
-    public int setOldParametros(Map<String, Macro> newMap, String var){
+    public int setOldParametros(Map<String, Macro> newMap, String var){ // Percorre macros de nivel superior em busca do parametro formal e seu correspondente valor real
 
         int i = nivel-1;
-        while(i >= 0){    // Percorre cada nível acima
+        while(i >= 0){    // Percorre cada nível acima, um por um até encontrar o parametro formal
             for (Map.Entry<String, Macro> entry : newMap.entrySet()) { // Procura por Macros da mesma ninhada
                 //String key = entry.getKey();
                 Macro value = entry.getValue();
-                //System.out.println("\nNinho value: " + value.getNinho());
                 if(value.ninho == this.ninho){
                     if(value.nivel == i){
-                        System.out.println("\nNivel value: " + value.getNivel() +" i value: " + i + " var: "+ var+ " Parametros: "+Arrays.toString(value.parametrosFormais));
                         String parametroFormal = "";
                         for(int j = 0; j < value.parametrosFormais.length; j++){
                             parametroFormal = value.parametrosFormais[j];
